ffe82777ada9ca3c00e1e03e0170b9dcadb3d9a9
RishiKrishnah <rishikrishnah@gmail.com>
2025-03-26 22:55:48 +0530

    Commiting vscode 2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function bookAppointment() {
     alert("Redirecting to the appointment booking page...");
-    window.location.href = "appointments.html"; // You can link to an appointment page
+    window.location.href = "Homepage.html"; // You can link to an appointment page
 }
